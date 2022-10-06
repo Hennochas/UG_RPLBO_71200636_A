dfb038fd903e832cf90e6d11ad8140663df149a7
Hennochas <78341298+Hennochas@users.noreply.github.com>
2022-10-06 18:33:41 +0700

    Add files via upload

---
+++ UG6_71200636/src/main/java/com/ug6/soal1/App.java
@@ -0,0 +1,25 @@
+package com.ug6.soal1;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "==========================================" );
+        System.out.println( "   Buat Vending Machine terlebih dahulu   " );
+        System.out.println( "==========================================" );
+        System.out.println("Kapasitas maxmal: ");
+
+        System.out.println("Apakah anda akan menambahkan barang sekarang?");
+        System.out.println( "ya (y) atau tidak(n)");
+        System.out.println("Nama barang: ");
+        System.out.println("Type barang: ");
+        System.out.println("1. Minuman");
+        System.out.println("1. Makanan");
+        System.out.println("1. Produk Digital");
+        System.out.println("Pilihan (1/2/3):");
+        System.out.println("Apakah anda ingin memasukkan nominal harga dan jumlah barang?");
+        System.out.println("ya(y) atau tidak(n)");
+        System.out.println("Kapasitas barang yang ada/dimiliki:");
+        System.out.println("Apakah anda ingin menambahkan lagi?");
+        System.out.println("Ya(y) atau tidak(n)");
+
+    }
+}
---
+++ UG6_71200636/src/main/java/com/ug6/soal1/Codes.java
@@ -0,0 +1,16 @@
+package com.ug6.soal1;
+
+public class Codes {
+    private String GOODSCODES="GD";
+    private String VENDINGMACHINESCODES="VM";
+    private String TYPE="DR";
+
+    public String generateGoodsCode(String type, String name){
+        return type;
+    }
+
+    public String generateVendingMachinesCode(int order){
+
+        return null;
+    }
+}
